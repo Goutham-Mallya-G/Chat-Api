9e7747508027c45c37e784defb6f29f39cc6e24e
mallya <gouthammallya2@gmail.com>
2026-07-17 03:59:00 +0530

    init commit

---
+++ src/test/java/com/mallya/chatapi/ChatapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mallya.chatapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
